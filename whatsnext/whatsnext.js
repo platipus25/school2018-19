45cb346c80e2cec66972ac932da73613e1d7a636
platipus25 <40722163+platipus25@users.noreply.github.com>
2018-09-12 12:37:08 -0700

    Update

--- whatsnext/whatsnext.js
+++ whatsnext/whatsnext.js
@@ -4,7 +4,9 @@ whatsnext = {
   getScheduleToday: function(now){
     if(!now) now = new Date()
     if(this.scheduleToday != null){
-      return this.scheduleToday
+      if(this.scheduleTodayDate == now.toDateString()){
+        return this.scheduleToday
+      }
     }
 
     var schedule = schedule2018_19()
@@ -22,8 +24,8 @@ whatsnext = {
 
     function timeToday(object, now){
       let otherNow = new Date(now.toString())
-      console.log("Other Now for timeToday is"+otherNow)
-      otherNow.setHours(object.hour, object.minute)
+      //console.log("Other Now for timeToday is "+otherNow)
+      otherNow.setHours(object.hour, object.minute, 0)
       return otherNow
     }
 
@@ -34,16 +36,104 @@ whatsnext = {
       periodObject.end = timeToday(periodObject.end, now)
       periodObject.info = periodObject.info
     }
-    console.log("Today's shcedule is ", todaysObject)
+    //console.log("Today's shcedule is: \n", todaysObject)
     this.scheduleToday = todaysObject
     this.scheduleTodayDate = now.toDateString()
-    console.log(schedule[day])
     return this.scheduleToday
+  },
+  getThisPeriod:function(now){
+    if(!now) now = new Date()
+    var scheduleToday = this.getScheduleToday()
+    for(period in scheduleToday){
+      let periodObj = scheduleToday[period]
+      if(periodObj.end > now){
+        if(periodObj.start <= now){
+          return periodObj
+        }
+      }
+    }
+    return {period:"none", start:null, end:null, info:null}
+  },
+  getNextPeriod:function(now){
+    if(!now) now = new Date()
+    var endOfThisPeriod = this.getThisPeriod(now).end
+    var fourMinutesFromEndOfThisPeriod = new Date(endOfThisPeriod.valueOf()+(4*60*1000))
+    var thisPeriod = this.getThisPeriod(fourMinutesFromEndOfThisPeriod)
+    return thisPeriod
+  },
+  isSchool: function(now){
+    if(!now) now = new Date()
+    var schedule = schedule2018_19()
+
+    // Is it summer break
+    if(now < schedule.school_year.start || now > schedule.school_year.end){
+      return [false, "Summer!!"]
+    }
+
+    // is it weekend
+    var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+    var day = days[now.getDay()]
+    if(day == "saturday" || day == "sunday"){
+      return [false, "Weekend"];
+    }
+
+    // is it a holiday or recess
+    var nextDayOff = this.getNextDayOff(now)
+    if(nextDayOff.rel == "now"){
+      return [false, nextDayOff.name];
+    }
+
+    // is it before or after school
+    var scheduleToday = this.getScheduleToday(now)
+    let first = (scheduleToday[1] || scheduleToday[2])
+    let last = (scheduleToday[8] || scheduleToday[7])
+    var start = first.start
+    var end = last.end
+    if(now < start) return [false, "Before School"];
+    if(now > end) return [false, "After School"];
+
+    return [true, "Yup"];
+  },
+  getNextDayOff:function(now){
+    if(!now) now = new Date();
+    var schedule = schedule2018_19()
+    for(let {name, date} of schedule.days_off){
+      if(date > now){
+        return {name:name, date:date, rel:"next"} 
+      }
+      if(date.toDateString() == now.toDateString()){
+          return {name:name, date:date, rel:"now"}
+      }
+    }
+    return "nope"
+  },
+  countdown:function(now, then){
+    var diff = then.getTime() - now.getTime();
+
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    diff -=  days * (1000 * 60 * 60 * 24);
+
+    var hours = Math.floor(diff / (1000 * 60 * 60));
+    diff -= hours * (1000 * 60 * 60);
+
+    var mins = Math.floor(diff / (1000 * 60));
+    diff -= mins * (1000 * 60);
+
+    var seconds = Math.floor(diff / (1000));
+    diff -= seconds * (1000);
+
+    var final = ""
+    if(days) final+=days+" days, "
+    if(hours) final+=hours+" hours, "
+    if(mins) final+=mins+" minutes, and "
+    final+=seconds +" seconds"
+    return final
   }
 }
 
-console.log(new Date(2018, 9, 10))
-
+//console.log(new Date(2018, 9, 10))
+//console.log(new Date(new Date().valueOf()+(4*60*1000)))
+//console.log(new Date())
 /*function schedule2018_19(){
   let schedule = {
     monday:{
