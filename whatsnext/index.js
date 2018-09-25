36364d9430f7cf8cb87d31b24681d65f13fa5bb0
Ravago Jones <ravagojones@icloud.com>
2018-09-24 20:41:23 -0700

    Whatsnext

---
+++ whatsnext/index.js
@@ -0,0 +1,170 @@
+const whatsnextLib = /*new whatsnext(new Date(2018, 9, 23, 10, 9))*/new whatsnext();
+console.log(whatsnextLib)
+
+whatsnextLib.periodInfo = {
+  1:{
+
+  },
+  2:{
+
+  },
+  3:{
+
+  },
+  4:{
+    teacher:"Mr McNulty",
+    subject:"Math"
+  },
+  5:{
+
+  },
+  6:{
+
+  },
+  7:{
+
+  },
+  8:{
+
+  }
+}
+
+var wigets = {
+  get now(){return (new Date().toDateString()+" "+new Date().toLocaleTimeString('en-US', {hour:"numeric", minute:"2-digit"}))},
+  get period(){
+    var periodObj = whatsnextLib.thisPeriod
+    var period;
+    if(periodObj.info == null){
+      period = periodObj.period
+    }else if(periodObj.period[0] == "p"){
+      period = "Passing Period"
+    }else{
+      period = "Period "+periodObj.period
+    }
+    if(period == "none") period = "School"
+    return period
+  },
+  get periodCountdown(){
+    if(periodEnd = whatsnextLib.thisPeriod.end == null){
+      return "0 minutes"
+    }
+    return countdown(new Date(), whatsnextLib.thisPeriod.end, countdown.HOURS | countdown.MINUTES).toHTML()
+  },
+  get nextPeriod(){
+    var period = whatsnextLib.nextPeriod.period
+    return period
+  },
+  get nextPeriodInfo(){
+    var period = whatsnextLib.nextPeriod
+    if(period.info != null){
+      return "<br>"+period.info.infoAsHTML()
+    }
+    return "none"
+  },
+  get nextPeriodStartTime(){
+    return  whatsnextLib.nextPeriod.start.toLocaleTimeString('en-US', {hour:"numeric", minute:"2-digit"})
+  },
+  get nextBreak(){
+    return whatsnextLib.nextDayOff.name
+  },
+  get nextBreakCountdown(){
+    return countdown(new Date(), whatsnextLib.nextDayOff.date, countdown.MONTHS | countdown.DAYS | countdown.HOURS).toHTML()
+  }
+}
+var fieldValuePairs = {
+  "Now":"now",
+  "period":"periodCountdown",
+  "Next Period":"nextPeriod",
+  "Details":"nextPeriodInfo",
+  "nextBreak":"nextBreakCountdown",
+}
+
+for(fieldMarker in fieldValuePairs){
+  let valueMarker = fieldValuePairs[fieldMarker]
+  var field = fieldMarker
+  var value = valueMarker
+  if(wigets.hasOwnProperty(fieldMarker)){
+    field = wigets[fieldMarker]
+  }
+  if(wigets.hasOwnProperty(valueMarker)){
+    value = wigets[valueMarker]
+  }
+  //console.log(fieldMarker, valueMarker)
+  console.log(field, value)
+  let makeDOM = function(field, value, tag, fieldMarker, valueMarker){
+    if(!tag) tag = "span"
+    var container = $("<div class='fieldContainer'></div>")
+    container.append($("<"+tag+" class='field' id='"+fieldMarker+"'>"+field+"</"+tag+">"))
+    container.append($("<span class='fieldValueColon'>: <span>"))
+    container.append($("<"+tag+" class='value' id='"+valueMarker+"'>"+value+"</"+tag+">"))
+    container.append($("<br>"))
+    console.log(container)
+    return container
+  }
+  $("#container").append(makeDOM(field, value, "span", fieldMarker, valueMarker))
+}
+
+function update(){
+  for(fieldMarker in fieldValuePairs){
+    let valueMarker = fieldValuePairs[fieldMarker]
+    var field = fieldMarker
+    var value = valueMarker
+    if(wigets.hasOwnProperty(fieldMarker)){
+      field = wigets[fieldMarker]
+    }
+    if(wigets.hasOwnProperty(valueMarker)){
+      value = wigets[valueMarker]
+    }
+    $("#"+fieldMarker).text(field)
+    $("#"+valueMarker).html(value)
+  }
+}
+
+/*
+for(field in fieldValuePairs){
+  let value = fieldValuePairs[field]
+  let fieldValuePair = {}
+  let fieldValue = field
+  let valuevalue = null
+  if(fieldValues.hasOwnProperty(field)){
+    fieldValue = fieldValues[field]
+    fieldValuePair[fieldValue] = null
+  }
+  if(fieldValues.hasOwnProperty(value)){
+    valuevalue = fieldValues[value]
+  }
+  fieldValuePair[fieldValue] = valuevalue
+  console.log(fieldValuePair)
+} */
+/*function update(){
+  for(fieldMarker in fieldValuePairs){
+
+  }
+}
+
+function update(){
+  $("#nowDate").text(new Date().toDateString())
+  $("#nowTime").text(new Date().toLocaleTimeString('en-US', {hour:"numeric", minute:"2-digit"}))
+  var periodObj = whatsnextLib.thisPeriod
+  var period;
+  if(periodObj.info == null){
+    period = periodObj.period
+  }else if(periodObj.period[0] == "p"){
+    period = "Passing Period"
+  }else{
+    period = "Period "+periodObj.period
+  }
+  $("#thisPeriod").text(period)
+  $("#endOfPeriodCountdown").text(countdown(new Date(), periodObj.end, countdown.HOURS | countdown.MINUTES))
+  var nextPeriod = whatsnextLib.nextPeriod
+  $("#period").text(nextPeriod.period)
+  $("#teacher").text(nextPeriod.info)
+  $("#subject").text(nextPeriod.info)
+  $("#room").text(nextPeriod.info)
+  $("#time").text(nextPeriod.start.toLocaleTimeString('en-US', {hour:"numeric", minute:"2-digit"}))
+  var nextBreak = whatsnextLib.nextDayOff
+  $("#nameOfBreak").text(nextBreak.name)
+  $("#dayOffCountdown").text(countdown(new Date(), nextBreak.date, countdown.MONTHS | countdown.DAYS | countdown.HOURS))
+}*/
+update()
+setInterval(update, 1000)
--- whatsnext/whatsnext.js
+++ whatsnext/whatsnext.js
@@ -1,24 +1,62 @@
-whatsnext = {
-  schedule:function(){return schedule2018_19()},
-  infoFunc:function(period){
-    return {}
-  },
-  setInfoFunction:function(callback){
-    if(typeof callback == "function"){
-      this.infoFunc = callback
+class whatsnext{
+  constructor(time){
+    if(time){
+      this.time = time
+      Object.defineProperty(this, 'now', {get:function(){return this.time}});
+      this.day = this.time.toLocaleDateString()
+    }else{
+      this.time = new Date()
+      this.day = null
     }
-  },
-  scheduleToday:null,
-  scheduleTodayDate:null,
-  getScheduleToday: function(now){
-    if(!now) now = new Date()
-    if(this.scheduleToday != null){
-      if(this.scheduleTodayDate == now.toDateString()){
-        return this.scheduleToday
+    this.theScheduleToday = null
+    this.periodInfo = {}
+  }
+  info(period){
+    if(!this.periodInfo.hasOwnProperty(period)) return {}
+    var thisPeriod = this.periodInfo[period]
+    let info = class{
+      constructor(period, thisRef){
+        this.period = period
+        this.parentRef = thisRef
+      }
+      get value(){
+        return this.parentRef.periodInfo[period]
+      }
+      get infoAsText(){
+        var formatted = ""
+        for(info in this.value){
+          var titleCase = info[0].toUpperCase()+info.slice(1, info.length)
+          formatted+=titleCase+": "+this.value[info]+"\n"
+        }
+        return formatted
+      }
+      infoAsHTML(tag){
+        if(!tag) tag = "span"
+        var formatted = "<div id='"+this.period+"' class='info'>"
+        for(info in this.value){
+          var titleCase = info[0].toUpperCase()+info.slice(1, info.length)
+          formatted+=("<"+tag+" class='field'>")+titleCase+("</"+tag+">")+": "+("<"+tag+" class='value'>")+this.value[info]+("</"+tag+">")+"<br>"
+        }
+        formatted = formatted.slice(0, formatted.length-4)
+        formatted+="</div>"
+        return formatted
       }
     }
+    return new info(period, this)
+  }
+  get now(){
+    return new Date()
+  }
+  get schedule_base(){
+    return schedule2018_19()
+  }
+  get schedule(){
+    var now = this.now
+    if(this.theScheduleToday != null){
+      return this.theScheduleToday
+    }
 
-    var schedule = schedule2018_19()
+    var schedule = this.schedule_base
     var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
     var day = days[now.getDay()]
     for(let {name, date} of schedule.minimum_days){
@@ -27,9 +65,12 @@ whatsnext = {
       }
     }
     console.log("Today is a "+day)
+    if(day == "sunday" || day == "saturday"){
+      return day
+    }
     var todaysObject = JSON.parse(JSON.stringify(schedule[day]))
     //console.log(todaysObject)
-    this.scheduleToday = {}
+    this.theScheduleToday = {}
 
     function timeToday(object, now){
       let otherNow = new Date(now.toString())
@@ -38,23 +79,23 @@ whatsnext = {
       return otherNow
     }
 
-    for(period in todaysObject){
+    for(let period in todaysObject){
       let periodObject = todaysObject[period]
       periodObject.start = timeToday(periodObject.start, now)
       periodObject.end = timeToday(periodObject.end, now)
       if(periodObject.info != null){
-        periodObject.info = this.infoFunc(period)
+        var thisRef = this
+        periodObject.info = this.info(period)
       }
     }
     //console.log("Today's shcedule is: \n", todaysObject)
-    this.scheduleToday = todaysObject
-    this.scheduleTodayDate = now.toDateString()
-    return this.scheduleToday
-  },
-  getThisPeriod:function(now){
-    if(!now) now = new Date()
-    var scheduleToday = this.getScheduleToday(now)
-    for(period in scheduleToday){
+    this.theScheduleToday = todaysObject
+    return this.theScheduleToday
+  }
+  get thisPeriod(){
+    var now = this.now
+    var scheduleToday = this.schedule
+    for(let period in scheduleToday){
       let periodObj = scheduleToday[period]
       if(periodObj.end > now){
         if(periodObj.start <= now){
@@ -63,18 +104,29 @@ whatsnext = {
       }
     }
     return {period:"none", start:null, end:null, info:null}
-  },
-  getNextPeriod:function(now){
-    if(!now) now = new Date()
-    var endOfThisPeriod = this.getThisPeriod(now).end
+  }
+  get nextPeriod(){
+    var now = this.now
+    var endOfThisPeriod = this.thisPeriod.end
     if(!endOfThisPeriod) return {period:"none", start:null, end:null, info:null}
     var fourMinutesFromEndOfThisPeriod = new Date(endOfThisPeriod.valueOf()+(4*60*1000))
-    var thisPeriod = this.getThisPeriod(fourMinutesFromEndOfThisPeriod)
-    return thisPeriod
-  },
-  isSchool: function(now){
-    if(!now) now = new Date()
-    var schedule = this.schedule()
+    var period = (function(now, schedule){
+      var scheduleToday = schedule
+      for(let period in scheduleToday){
+        let periodObj = scheduleToday[period]
+        if(periodObj.end > now){
+          if(periodObj.start <= now){
+            return periodObj
+          }
+        }
+      }
+      return {period:"none", start:null, end:null, info:null}
+    })(fourMinutesFromEndOfThisPeriod, this.schedule)
+    return period
+  }
+  get isSchool(){
+    var now = this.now
+    var schedule = this.schedule_base
 
     // Is it summer break
     if(now < schedule.school_year.start || now > schedule.school_year.end){
@@ -89,13 +141,13 @@ whatsnext = {
     }
 
     // is it a holiday or recess
-    var nextDayOff = this.getNextDayOff(now)
+    var nextDayOff = this.nextDayOff
     if(nextDayOff.rel == "now"){
       return [false, nextDayOff.name];
     }
 
     // is it before or after school
-    var scheduleToday = this.getScheduleToday(now)
+    var scheduleToday = this.schedule
     let first = (scheduleToday[1] || scheduleToday[2])
     let last = (scheduleToday[8] || scheduleToday[7])
     var start = first.start
@@ -104,41 +156,20 @@ whatsnext = {
     if(now > end) return [false, "After School"];
 
     return [true, "Yup"];
-  },
-  getNextDayOff:function(now){
-    if(!now) now = new Date();
-    var schedule = this.schedule()
+  }
+
+  get nextDayOff(){
+    var now = this.now
+    var schedule = this.schedule_base
     for(let {name, date} of schedule.days_off){
       if(date > now){
-        return {name:name, date:date, rel:"next"} 
+        return {name:name, date:date, rel:"next"}
       }
       if(date.toDateString() == now.toDateString()){
           return {name:name, date:date, rel:"now"}
       }
+      //console.log(name, date)
     }
     return "nope"
-  },
-  countdown:function(now, then){
-    var diff = then.getTime() - now.getTime();
-
-    var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    diff -=  days * (1000 * 60 * 60 * 24);
-
-    var hours = Math.floor(diff / (1000 * 60 * 60));
-    diff -= hours * (1000 * 60 * 60);
-
-    var mins = Math.floor(diff / (1000 * 60));
-    diff -= mins * (1000 * 60);
-
-    var seconds = Math.floor(diff / (1000));
-    diff -= seconds * (1000);
-
-    var final = ""
-    if(days) final+=days+" days, "
-    if(hours) final+=hours+" hours, "
-    if(mins) final+=mins+" minutes, and "
-    final+=seconds +" seconds"
-    return final
   }
 }
-//whatsnext.getScheduleToday(new Date())
